2bed6f36b542de7feeaba94c977ce9fc55811a55
Javi <javiermoyasimo@gmail.com>
2023-01-18 11:31:08 +0100

    feat: first steps in form and table

---
+++ contractstable/src/containers/Table/Table.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Table.scss'
+
+
+const Table = () => {
+    return (
+        <div className='tableDesign'>
+            Soy Table
+        </div>
+    )
+}
+
+export default Table;
