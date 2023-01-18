909443f67af9ec4f1aff9f763db4b754c28f2ea3
Javi <javiermoyasimo@gmail.com>
2023-01-18 11:34:56 +0100

    feat: services added. Apicalls added. Provider in index.js added

---
+++ contractstable/src/services/apiCalls.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+// export const loginUser = async (user) => {//el user se lo he metido cuando he implementado la funcion en la otra funcion logMe
+//     let res = await axios.post("endpoint del alumno", user)// Pasariamos un body, que en este caso es user.
+//     return res;
+// };
