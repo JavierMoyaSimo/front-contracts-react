e299ab917b56eee939d58627b904b96fb5d79f22
Javi <javiermoyasimo@gmail.com>
2023-01-18 11:27:27 +0100

    feat: In app.js, imported browserRouter, Router and routes

--- contractstable/src/App.js
+++ contractstable/src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Table from "./containers/Table/Table";
+
+
 
 function App() {
   return (
     <div className="App">
-      
+
+      <BrowserRouter>
+
+        <Routes>
+
+          <Route path='/' element={<Table />} />
+
+        </Routes>
+
+      </BrowserRouter>
+
     </div>
   );
 }
